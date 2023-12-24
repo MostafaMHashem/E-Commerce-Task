9e01e1d561d631863368bdacb18d0e9b409e3a96
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2023-12-24 14:42:15 +0200

    require our myfatoorah package

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Fatoorahpayment\Gatewayintegration\GatewayintegrationServiceProvider;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
@@ -159,7 +160,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        GatewayintegrationServiceProvider::class,
         /*
          * Application Service Providers...
          */
